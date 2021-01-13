c0b0a46421d4ac87ff6cfb8f4a59249ab862dddf
William Hayward <fml3956303@fed.cclrc.ac.uk>
2021-01-13 16:05:08 +0000

    changing pom file - testing jenkins

--- Pet_Clinic_Website_Java/push_img_to_local_registry.bat
+++ Pet_Clinic_Website_Java/push_img_to_local_registry.bat
@@ -1,8 +1,13 @@
 ECHO OFF
 
+docker stop testcontainer
+docker rm testcontainer
+
 @REM Couldn't use "- < mavenjdkagent.Dockerfile" as the "ADD" command
 @REM wouldn't find the directory to copy from.
 docker build -t localhost:5000/docker-agent-mavenjdk:v1 .
 docker push localhost:5000/docker-agent-mavenjdk:v1
 
-docker run localhost:5000/docker-agent-mavenjdk:v1
+docker run -d --name testcontainer localhost:5000/docker-agent-mavenjdk:v1
+
+docker exec -it testcontainer bash
