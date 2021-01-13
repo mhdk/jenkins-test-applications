b56c493af150c58ae189d35a4c56909a476be3a9
William Hayward <fml3956303@fed.cclrc.ac.uk>
2021-01-13 11:24:24 +0000

    Making changes to dockerfile and pom file to include dependencies in the image. Testing with jenkins.

--- Pet_Clinic_Website_Java/push_img_to_local_registry.bat
+++ Pet_Clinic_Website_Java/push_img_to_local_registry.bat
@@ -1,6 +1,8 @@
 ECHO OFF
 
-docker build -t localhost:5000/docker-agent-mavenjdk:v1 - < mavenjdkagent.Dockerfile
+@REM Couldn't use "- < mavenjdkagent.Dockerfile" as the "ADD" command
+@REM wouldn't find the directory to copy from.
+docker build -t localhost:5000/docker-agent-mavenjdk:v1 .
 docker push localhost:5000/docker-agent-mavenjdk:v1
 
-@REM docker run localhost:5000/docker-agent-mavenjdk:v1
+docker run localhost:5000/docker-agent-mavenjdk:v1
