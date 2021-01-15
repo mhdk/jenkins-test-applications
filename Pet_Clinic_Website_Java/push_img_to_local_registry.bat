8bb4db7f8e3a6a15396d20a444f6cd682b8ab958
William Hayward <fml3956303@FED.CCLRC.AC.UK>
2021-01-15 16:02:24 +0000

    Now works only if jenkins job run as root... also modifying pom file to uncomment repositories and re test

--- Pet_Clinic_Website_Java/push_img_to_local_registry.bat
+++ Pet_Clinic_Website_Java/push_img_to_local_registry.bat
@@ -5,7 +5,7 @@ docker rm testcontainer
 
 @REM Couldn't use "- < mavenjdkagent.Dockerfile" as the "ADD" command
 @REM wouldn't find the directory to copy from.
-docker build -t localhost:5000/docker-agent-mavenjdk:v1 .
+docker build --no-cache -t localhost:5000/docker-agent-mavenjdk:v1 .
 docker push localhost:5000/docker-agent-mavenjdk:v1
 
 docker run -d --name testcontainer localhost:5000/docker-agent-mavenjdk:v1
