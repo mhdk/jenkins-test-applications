b9630f496dbc5e9c201ed00b383dba879077f450
William Hayward <fml3956303@FED.CCLRC.AC.UK>
2021-01-29 17:59:41 +0000

    Adding k8s test jenkinsfile and moving hello world files into same folder

--- Pet_Clinic_Website_Java/test/push_img_to_local_registry.bat
+++ Pet_Clinic_Website_Java/test/push_img_to_local_registry.bat
@@ -5,9 +5,19 @@ docker rm testcontainer
 
 @REM Couldn't use "- < mavenjdkagent.Dockerfile" as the "ADD" command
 @REM wouldn't find the directory to copy from. 
-docker build --no-cache -t localhost:5000/test123:v1 .
+@REM docker build --no-cache -t localhost:5000/test123:v1 .
 @REM docker push localhost:5000/test123:v1
 
-docker run -d --name testcontainer localhost:5000/test123:v1
+@REM docker run -d --name testcontainer localhost:5000/test123:v1
+
+
+docker run -d ^
+           --name testcontainer ^
+           --rm ^
+           --group-add 0 ^
+           -v //var/run/docker.sock:/var/run/docker.sock ^
+           -v //usr/local/bin/docker:/usr/local/bin/docker ^
+           jenkins/jenkins:lts
+
 
 docker exec -it testcontainer bash
